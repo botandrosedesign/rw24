2bb35d905994ce6fb6c2ba288bbf37b99c2f8753
Michael Gubitosa <gubs@botandrose.com>
2010-05-31 14:32:10 -0700

    application layout build

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,25 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  
+  def link_to_current(name, options = {}, html_options = {})
+    url = case options
+      when String
+        options
+      when :back
+        @controller.request.env["HTTP_REFERER"] || 'javascript:history.back()'
+      else
+        self.url_for(options)
+      end
+    condition = html_options.delete(:if) || url == request.request_uri
+
+    if condition
+      if html_options[:class]
+        html_options[:class] += ' current'
+      else
+        html_options[:class] = 'current'
+      end
+    end
+    link_to name, options, html_options
+  end
+  
 end
