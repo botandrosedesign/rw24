9a1d535cf23a067bdfb7d2e206ff0a8aa14b08ae
Micah Geisel <micah@botandrose.com>
2010-06-10 15:57:45 -0700

    use metal for static content. fuck adva's routing filters.

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,14 +1,5 @@
 class StaticController < ApplicationController
   def dispatch
-    view_template_path = "/static/"+params[:path].join("/")
-    begin
-      render view_template_path, :layout => true
-    rescue ActionView::MissingTemplate
-      begin
-        render view_template_path+"/index", :layout => true
-      rescue ActionView::MissingTemplate
-        raise ActiveRecord::RecordNotFound
-      end
-    end
+    render "/static"+request.env["rack.static_path"], :layout => true
   end
 end
