bdec28e2a6f9c6dc0d8a968c040dcfcf5d2eaa12
Michael Gubitosa <gubs@botandrose.com>
2010-06-12 17:35:36 -0700

    admin races and rider page builds

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,6 +2,6 @@ class StaticController < BaseController
   before_filter :guess_section
 
   def dispatch
-    render "/static"+request.env["rack.static_path"], :layout => true
+    render "/static"+request.env["rack.static_path"], :layout => false
   end
 end
