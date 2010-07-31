8fc09189bde0be21eb3d2983696674d90e674a77
Micah Geisel <originofstorms@gmail.com>
2010-07-30 21:19:06 -0500

    cache leader board via rspec integration test.

---
+++ spec/integration/leader_board_caches_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+require "rack/test"
+
+describe "LeaderBoardCaches" do
+  it "caches" do
+    include Rack::Test::Methods
+    get "/leader-board"
+    f = File.open "public/leader-board.html", "w"
+    f << response.body
+    f.close
+  end
+end
