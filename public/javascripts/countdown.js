94f7a59d3bfb4066b54c420f6b3ab8aaa481340b
Micah Geisel <micah@botandrose.com>
2011-07-28 20:41:12 -0500

    Revert "Revert "optimize countdown javascript.""
    This reverts commit 68f7e68fe5242c97ee30d881df51ba5479754f21.

--- public/javascripts/countdown.js
+++ public/javascripts/countdown.js
@@ -1,45 +1,59 @@
-$(function() {
-  var update_date = function() {
-    var oneMinute = 60 * 1000;
-    var oneHour = oneMinute * 60;
-    var oneDay = oneHour * 24;
-    var oneYear = oneDay * 365;
-
-    var now = new Date().valueOf();
+var countdown = {
+  init: function() {
     // "Sat Jul 31 01:00:00 UTC 2010"
-    var start = new Date(Date.UTC(2011, 6, 30, 0, 0, 0)).valueOf();
+    this.start = new Date(Date.UTC(2011, 6, 30, 0, 0, 0)).valueOf();
     // "Sat Aug 01 01:00:00 UTC 2010"
-    var finish = new Date(Date.UTC(2011, 6, 31, 0, 0, 0)).valueOf();
+    this.finish = new Date(Date.UTC(2011, 6, 31, 0, 0, 0)).valueOf();
     // "Sat Jul 31 01:00:00 UTC 2011"
-    var next = new Date(Date.UTC(2012, 6, 27, 0, 0, 0)).valueOf();
-    
-    var current = start < now && now < finish
+    this.next = new Date(Date.UTC(2012, 6, 27, 0, 0, 0)).valueOf();
+    this.now = new Date().valueOf();
 
-    if(now > finish) {
-      start = next;
+    this.oneMinute = 60 * 1000;
+    this.oneHour = this.oneMinute * 60;
+    this.oneDay = this.oneHour * 24;
+    this.oneYear = this.oneDay * 365;
+
+    this.old_current = null;
+
+    setInterval(this.render, 1000);
+    this.render()
+  },
+
+  render: function() {
+    self = countdown;
+    self.now += 1000;
+
+    if(self.now > self.finish) {
+      self.start = self.next;
     }
 
-    var diff = Math.abs(start - now);
+    var diff = Math.abs(self.start - self.now);
 
-    var countdownDay = Math.floor(diff / oneDay);
-    var countdownHrs = Math.floor(diff % oneDay / oneHour);
-    var countdownMin = Math.floor(diff % oneHour / oneMinute);
-    var countdownSec = Math.floor(diff % oneMinute / 1000);
+    var countdownDay = Math.floor(diff / self.oneDay);
+    var countdownHrs = Math.floor(diff % self.oneDay / self.oneHour);
+    var countdownMin = Math.floor(diff % self.oneHour / self.oneMinute);
+    var countdownSec = Math.floor(diff % self.oneMinute / 1000);
 
     $("#day b").html(countdownDay < 10 ? "0"+countdownDay.toString() : countdownDay);
     $("#hrs b").html(countdownHrs < 10 ? "0"+countdownHrs.toString() : countdownHrs);
     $("#min b").html(countdownMin < 10 ? "0"+countdownMin.toString() : countdownMin);
     $("#sec b").html(countdownSec < 10 ? "0"+countdownSec.toString() : countdownSec);
 
-    if(current) {
-      $("#countdown p.countup").hide();
-      $("#countdown p.countdown").show();
-    } else {
-      $("#countdown p.countup").show();
-      $("#countdown p.countdown").hide();
+    var current = self.start < self.now && self.now < self.finish;
+    if(current != self.old_current) {
+      if(current) {
+        $("#countdown p.countup").hide();
+        $("#countdown p.countdown").show();
+      } else {
+        $("#countdown p.countup").show();
+        $("#countdown p.countdown").hide();
+      }
     }
     $("#countdown").css("visibility", "");
-  };
+    self.old_current = current;
+  }
+}
 
-  setInterval(update_date, 1000);
+$(function() {
+  countdown.init();
 });
