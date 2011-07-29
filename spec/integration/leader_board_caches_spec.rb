c10dadbd6254e37b0b1232b556b6e9b38b458603
Michael Gubitosa <gubs@botandrose.com>
2011-07-28 20:53:18 -0500

    Revert "reenable leader_board_caches spec."
    This reverts commit 073475fca756db04394443093f9a1f4602f9bc66.

--- spec/integration/leader_board_caches_spec.rb
+++ spec/integration/leader_board_caches_spec.rb
@@ -1,14 +1,14 @@
-ENV['RAILS_ENV'] = "production"
-
-require 'spec_helper'
-require "rack/test"
-
-describe "LeaderBoardCaches" do
-  it "caches" do
-    include Rack::Test::Methods
-    get "/leader-board"
-    f = File.open "public/leader-board.html", "w"
-    f << response.body
-    f.close
-  end
-end
+# ENV['RAILS_ENV'] = "production"
+# 
+# require 'spec_helper'
+# require "rack/test"
+# 
+# describe "LeaderBoardCaches" do
+#   it "caches" do
+#     include Rack::Test::Methods
+#     get "/leader-board"
+#     f = File.open "public/leader-board.html", "w"
+#     f << response.body
+#     f.close
+#   end
+# end
