7b4fb589ef506ddde73c47d06f467af639d6562a
Micah Geisel <micah@botandrose.com>
2012-05-03 22:48:21 -0700

    fix date in registration confirmation email.

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -2,7 +2,7 @@ class RegistrationMailer < ActionMailer::Base
   def registration(team)
     recipients   team.riders.collect(&:email).reject(&:blank?)
     from         "register@riverwest24.com"
-    subject      "Riverwest 24 2010 - Team signup confirmation"
+    subject      "Riverwest 24 #{Date.today.year} - Team signup confirmation"
     body         :team => team
     content_type "text/html"
   end
