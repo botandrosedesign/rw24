4f4ce3676e6bc1f86f4bcdf3a6bdf6e48f286a72
Micah Geisel <micah@botandrose.com>
2012-05-03 22:20:01 -0700

    use newer ree on production.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 set :application, "rw24"
 set :asset_paths, "public/userfiles"
 role :production, "www@riverwest24.com:22022"
-set :rvm_ruby_string, "ree@rw24"
+set :rvm_ruby_string, "ree-2011.03@rw24"
 
 desc "Clear the static cache"
 task :clear_cache, :roles => :production do
