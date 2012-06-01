8cbd862d7a74bc10c52b5d75f2c008ed4173e578
Micah Geisel <micah@botandrose.com>
2012-06-01 12:42:45 -0700

    let's try that again.

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -1,11 +1,11 @@
 class Mailer < ActionMailer::Base
-  default :from => "register@riverwest24.com", :content_type => "text/plain"
+  default :from => "register@riverwest24.com"
 
   def confirmation_email team
     @team = team
     mail :to => team.captain_email,
       :cc => team.lieutenant_emails,
-      :subject => "Riverwest 24 #{team.race.year}: #{team.position} - #{team.name}"
-      "RW24 #{team.race.year} Confirmation: #{team.name} (##{team.position}) - #{team.category} - Shirts: #{team.shirt_sizes}"
+      :content_type => "text/plain",
+      :subject => "RW24 #{team.race.year} Confirmation: #{team.name} (##{team.position}) - #{team.category} - Shirts: #{team.shirt_sizes}"
   end
 end
