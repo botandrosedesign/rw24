db6ac67dacfbf788cc661e4f207b85e5bcbba6e9
Micah Geisel <micah@botandrose.com>
2012-06-01 12:15:09 -0700

    can select all 'not emailed' teams.

--- vendor/adva/engines/adva_cms/public/javascripts/adva_cms/admin/select_all_none.js
+++ vendor/adva/engines/adva_cms/public/javascripts/adva_cms/admin/select_all_none.js
@@ -4,6 +4,12 @@ $(function() {
     return false
   });
 
+  $("#select_not_emailed").click(function() {
+    $(this).parents("form").find("input:checkbox").attr("checked", false)
+    $(this).parents("form").find(".not_emailed").parent().parent().find("input:checkbox").attr("checked", true)
+    return false
+  });
+
   $("#select_none").click(function() {
     $(this).parents("form").find("input:checkbox").attr("checked", false)
     return false
