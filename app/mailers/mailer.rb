322dda1d4b846c7b44cd0d603a4b1fef009490fc
Micah Geisel <micah@botandrose.com>
2012-06-01 17:39:43 -0700

    do the defaults mail settings not work?

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -1,10 +1,9 @@
 class Mailer < ActionMailer::Base
-  default :from => "register@riverwest24.com"
-
   def confirmation_email team
     @team = team
     mail :to => team.captain_email,
       :cc => team.lieutenant_emails,
+      :from => "register@riverwest24.com",
       :content_type => "text/plain",
       :subject => "RW24 #{team.race.year} Confirmation: #{team.name} (##{team.position}) - #{team.category} - Shirts: #{team.shirt_sizes}"
   end
