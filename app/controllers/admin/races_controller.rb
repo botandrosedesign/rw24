fed3c5f1c1ad041cf49b216952ddec68cac6900a
Micah Geisel <micah@botandrose.com>
2012-07-25 15:06:01 -0700

    update expiration logic.

--- app/controllers/admin/races_controller.rb
+++ app/controllers/admin/races_controller.rb
@@ -11,7 +11,7 @@ class Admin::RacesController < Admin::BaseController
   def create
     @race = Race.new params[:race]
     if @race.save
-      expire_all_cached_pages
+      @site.touch # expire all cached pages
       redirect_to [:admin, @site, @race, :teams], :notice => "Race created!"
     else
       render :new
@@ -21,7 +21,7 @@ class Admin::RacesController < Admin::BaseController
   def update
     @race = Race.find params[:id]
     @race.update_attributes params[:race]
-    expire_all_cached_pages
+    @site.touch # expire all cached pages
     redirect_to [:admin, @site, @race, :teams]
   end
 end
