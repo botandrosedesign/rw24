39bb96a53f7eb6c389a43ebb5b16527299f16e21
Micah Geisel <micah@botandrose.com>
2014-07-23 16:58:36 -0700

    fix database cleaner configuration.

--- features/support/database_cleaner.rb
+++ features/support/database_cleaner.rb
@@ -1,9 +1,16 @@
-# encoding: utf-8
+require 'database_cleaner'
+require 'database_cleaner/cucumber'
 
 ActiveRecord::Base.shared_connection = ActiveRecord::Base.connection
 ActiveRecord::Base.descendants.each do |model|
   model.shared_connection = model.connection
 end
- 
+
 DatabaseCleaner.clean_with :truncation
-load "db/seeds.rb"
+
+DatabaseCleaner.strategy = :transaction
+
+Around do |scenario, block|
+  DatabaseCleaner.cleaning(&block)
+end
+
