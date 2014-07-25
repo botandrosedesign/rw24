866a5c7e0b148e98e27811ee8576221ca11cb406
Micah Geisel <micah@botandrose.com>
2014-07-25 12:19:22 -0500

    minor refactor.

--- app/controllers/admin/bonuses_controller.rb
+++ app/controllers/admin/bonuses_controller.rb
@@ -2,28 +2,28 @@ class Admin::BonusesController < Admin::BaseController
   before_filter :set_race
 
   def show
-    @bonus = Bonus.find_by_race_and_id @race, params[:id]
-    @bonuses = Point.where(:bonus_id => params[:id], :race_id => @race.id, :category => "Bonus").includes(:team).reorder("teams.position")
+    @bonus = Bonus.find_by_race_and_id(@race, params[:id])
+    @bonuses = Point.bonuses.where(bonus_id: params[:id], race_id: @race.id).includes(:team).reorder("teams.position")
   end
 
   def new
-    @bonus = Bonus.new :points => 2
+    @bonus = Bonus.new(points: 2)
   end
 
   def create
     params[:bonus][:key] = SecureRandom.hex(8)
     @race.bonuses << params[:bonus]
     @race.save
-    redirect_to [:new, :admin, @site, @race, :bonus], :notice => "Bonus added! Add another?"
+    redirect_to [:new, :admin, @site, @race, :bonus], notice: "Bonus added! Add another?"
   end
 
-private
+  private
 
   def set_menu
     @menu = Menus::Admin::Teams.new
   end
 
   def set_race
-    @race = Race.find params[:race_id]
+    @race = Race.find(params[:race_id])
   end
 end
