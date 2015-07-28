a6062015c36caed5263382654b0e4dd07695a943
Micah Geisel <micah@botandrose.com>
2015-07-28 11:29:49 -0700

    update rails config files.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,4 +5,6 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w(leader_board.js points.js)
+Rails.application.config.assets.precompile += %w(admin.css admin-score.css ie.css bonus_logger.css)
+
