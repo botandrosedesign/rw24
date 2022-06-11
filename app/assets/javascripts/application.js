9ccb243e3a40a48531b2abbc7a28896c01d1a811
Micah Geisel <micah@botandrose.com>
2022-06-11 13:42:16 +0200

    move countdown to importmap.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,7 +2,6 @@
 //= require jquery_ujs
 //= require google_analytics
 //= require account_nav
-//= require countdown
 //= require ceebox
 //= require adva_cms/cookie
 //= require adva_cms/jquery.flash
