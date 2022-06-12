7c05bfc98fd191d0e8e650fe6b039203f60e1237
Micah Geisel <micah@botandrose.com>
2022-06-12 14:47:18 +0200

    split one controller into many.

--- app/javascript/controllers/ranker_controller.js
+++ app/javascript/controllers/ranker_controller.js
@@ -1,33 +1,26 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = []
-  static values = {}
+  static targets = ["leader", "winners"]
+  static values = { initial: String }
 
   connect() {
-    this.initials = ["A","B","S","M","F","T"]
-    this.pickLeaders()
-    this.pickWinners()
+    if(this.hasLeaderTarget) { this.pickLeader() }
+    if(this.hasWinnersTarget) { this.pickWinners() }
   }
 
-  pickLeaders() {
-    this.initials.forEach(initial => {
-      const leader = this.leadersByCategory(initial)[0]
-      const leaderElement = this.element.querySelector(`#${initial.toLowerCase()}_leader`)
-      if(leaderElement) { leaderElement.innerHTML = leader }
-    })
+  pickLeader() {
+    const leader = this.#leadersByCategory(this.initialValue)[0]
+    this.leaderTarget.innerHTML = leader
   }
 
   pickWinners() {
-    this.initials.forEach(initial => {
-      const winningThree = this.leadersByCategory(initial).slice(0, 3)
-      const winners = winningThree.map(winner => `<b>${winner}</b>`).join("<br>")
-      const winnerElement = this.element.querySelector(`#${initial.toLowerCase()}_winners`)
-      if(winnerElement) { winnerElement.innerHTML = winners }
-    })
+    const winningThree = this.#leadersByCategory(this.initialValue).slice(0, 3)
+    const winners = winningThree.map(winner => `<b>${winner}</b>`).join("<br>")
+    this.winnersTarget.innerHTML = winners
   }
 
-  leadersByCategory(initial) {
+  #leadersByCategory(initial) {
     const list = document.querySelectorAll(`#teams tr.${initial} td:nth-child(3)`)
     return Array.from(list).map(e => e.innerText)
   }
