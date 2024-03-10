ff4f0cab1527c6a3992038bdb3134d445afe8e88
Micah Geisel <micah@botandrose.com>
2024-03-10 10:21:44 -0600

    schema.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_05_181206) do
+ActiveRecord::Schema[7.1].define(version: 2023_07_05_181206) do
   create_table "accounts", id: :integer, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", options: "ENGINE=InnoDB ROW_FORMAT=DYNAMIC", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: nil
