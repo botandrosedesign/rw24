25002496cfc87b46f848215ecfa40cc1fe645fc4
Micah Geisel <micah@botandrose.com>
2024-06-22 18:40:00 +0200

    daily db backups.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,4 @@
-# Use this file to easily define all of your cron jobs.
+n# Use this file to easily define all of your cron jobs.
 #
 # It's helpful, but not entirely necessary to understand cron before proceeding.
 # http://en.wikipedia.org/wiki/Cron
@@ -23,5 +23,9 @@ set :output, "log/whenever.log"
 
 every 1.minute do
   rake "cache_leaderboard"
+end
+
+every 1.day do
+  rake "db:backup"
 end
 
