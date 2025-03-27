d7d3091365cc2985ac05354bcff45660ffdd5eb8
Micah Geisel <micah@botandrose.com>
2025-03-27 14:07:11 -0400

    update exception notification.

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -12,10 +12,41 @@ ExceptionNotification.configure do |config|
   end
 
   config.add_notifier :email, {
-    email_prefix: "[riverwest24.com] ",
+    email_prefix: "[#{File.basename(Dir.pwd)}] ",
     exception_recipients: "micah@botandrose.com",
-    smtp_settings: Rails.application.credentials[:exception_notification_smtp_settings],
+    sender_address: "errors@bardtracker.com",
+    smtp_settings: Rails.application.credentials.exception_notification_smtp_settings,
   }
+end
+
+if defined?(Rake::Application)
+  Rake::Application.prepend Module.new {
+    def display_error_message error
+      ExceptionNotifier.notify_exception(error)
+      super
+    end
+
+    def invoke_task task_name
+      super
+    rescue RuntimeError => exception
+      if exception.message.starts_with?("Don't know how to build task")
+        ExceptionNotifier.notify_exception(exception)
+      end
+      raise exception
+    end
+  }
+end
+
+class ActiveJob::Base
+  rescue_from StandardError do |exception|
+    ExceptionNotifier.notify_exception(exception, env: Rails.env, data: {
+      job_class: self.class.name,
+      job_id: job_id,
+      arguments: arguments,
+      queue_name: queue_name,
+    })
+    raise exception
+  end
 end
 
 Delayed::Worker.prepend Module.new {
