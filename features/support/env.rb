ea4d1417204c63dcbd61fd09705c83768813aa1c
Micah Geisel <micah@botandrose.com>
2026-03-14 14:39:43 -0600

    modernize cuprite setup.

--- features/support/env.rb
+++ features/support/env.rb
@@ -7,9 +7,46 @@ Capybara.register_driver(:cuprite) do |app|
   Capybara::Cuprite::Driver.new(app, {
     window_size: [1200, 2048],
     timeout: 20,
+    process_timeout: 20,
     js_errors: true,
-    headless: true,
+    logger: FerrumLogger.new,
+    # headless: false,
   })
+end
+
+class FerrumLogger
+  def puts(log_str)
+    return if log_str.nil?
+
+    _log_symbol, _log_time, log_body_str = log_str.to_s.strip.split(' ', 3)
+
+    return if log_body_str.nil?
+
+    begin
+      log_body = JSON.parse(log_body_str)
+    rescue JSON::ParserError
+      return Kernel.puts log_body_str
+    end
+
+    case log_body['method']
+    when 'Runtime.consoleAPICalled'
+      log_body['params']['args'].each do |arg|
+        case arg['type']
+        when 'string'
+          Kernel.puts arg['value']
+        when 'object'
+          Kernel.puts arg['preview']['properties'].map { |x| [x["name"], x["value"]] }.to_h
+        end
+
+      end
+
+    when 'Runtime.exceptionThrown'
+      # noop, this is already logged because we have "js_errors: true" in cuprite.
+
+    when 'Log.entryAdded'
+      Kernel.puts "#{log_body['params']['entry']['url']} - #{log_body['params']['entry']['text']}"
+    end
+  end
 end
 
 Capybara.server = :puma, { Silent: true }
