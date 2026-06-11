7bd94ee2c56b41c19d51e6c918add6b0ff971ed0
Micah Geisel <micah@botandrose.com>
2026-06-11 12:36:19 +0100

    ignore some errors.

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -3,7 +3,10 @@ require "exception_notification/rails"
 ExceptionNotification.configure do |config|
   # Ignore additional exception types.
   # ActiveRecord::RecordNotFound, AbstractController::ActionNotFound and ActionController::RoutingError are already added.
-  # config.ignored_exceptions += %w{ActionView::TemplateError CustomError}
+  config.ignored_exceptions += %w[
+    ActionController::BadRequest
+    ActionDispatch::Http::Parameters::ParseError
+  ]
 
   # Adds a condition to decide when an exception must be ignored or not.
   # The ignore_if method can be invoked multiple times to add extra conditions.
